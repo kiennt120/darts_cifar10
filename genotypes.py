fa67115f800ad1a54f0fb5cb89447358f076b5ef
kiennt120 <kyriejad23@gmail.com>
2023-02-09 12:44:57 +0700

    rm wrong genotype

--- genotypes.py
+++ genotypes.py
@@ -85,11 +85,11 @@ DARTS_V2 = Genotype(
             ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5]
 )
 
-DARTS_result = Genotype(
-    normal=[('sep_conv_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('dil_conv_5x5', 1), ('sep_conv_5x5', 3),
-            ('avg_pool_3x3', 1), ('dil_conv_5x5', 0), ('dil_conv_5x5', 3)], normal_concat=range(2, 6),
-    reduce=[('avg_pool_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 2), ('skip_connect', 1), ('sep_conv_3x3', 3),
-            ('max_pool_3x3', 2), ('sep_conv_3x3', 2), ('avg_pool_3x3', 1)], reduce_concat=range(2, 6)
-)
+# DARTS_result = Genotype(
+#     normal=[('sep_conv_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('dil_conv_5x5', 1), ('sep_conv_5x5', 3),
+#             ('avg_pool_3x3', 1), ('dil_conv_5x5', 0), ('dil_conv_5x5', 3)], normal_concat=range(2, 6),
+#     reduce=[('avg_pool_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 2), ('skip_connect', 1), ('sep_conv_3x3', 3),
+#             ('max_pool_3x3', 2), ('sep_conv_3x3', 2), ('avg_pool_3x3', 1)], reduce_concat=range(2, 6)
+# )
 
-DARTS = DARTS_result
+DARTS = DARTS_V2
